9dfca5448f05d049e55e44e483d269782fdf12c3
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-04 14:40:14 +0900

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,24 @@
 package PACKAGE_NAME;
 
-public class Employee {
+public class Employee extends Worker {
+
+    private long employeeId;
+    private String hireDate;
+
+    public Employee(String name, String birthDate, String endDate, long employeeId) {
+        super(name, birthDate, endDate);
+        this.employeeId = employeeId;
+    }
+
+    public Employee(String name, String birthDate, String endDate) {
+        super(name, birthDate, endDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", hireDate='" + hireDate + '\'' +
+                "} " + super.toString();
+    }
 }
