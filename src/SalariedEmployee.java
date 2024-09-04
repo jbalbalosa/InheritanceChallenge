d76491ab7f7ecb581cfa85633039c2e8a0cbb6f5
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-04 14:40:55 +0900

    Update SalariedEmployee.java

--- src/SalariedEmployee.java
+++ src/SalariedEmployee.java
@@ -1,4 +1,40 @@
 package PACKAGE_NAME;
 
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee{
+
+    private double annualSalary;
+    private boolean isRetired;
+
+    public SalariedEmployee(String name, String birthDate, String endDate, long employeeId) {
+        super(name, birthDate, endDate, employeeId);
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    public boolean getIsRetired() {
+        return isRetired;
+    }
+
+    public void setIsRetired(boolean isRetired) {
+        this.isRetired = isRetired;
+    }
+
+    public void retire(){
+        System.out.println(getName().toString() + " is " + (isRetired ? "Retired" : "Not yet retired"));
+    }
+
+//    @Override
+//    public String toString() {
+//        return "SalariedEmployee{" +
+//                "annualSalary=" + annualSalary +
+//                ", isRetired=" + isRetired +
+//                "} " + super.toString();
+//    }
 }
+
